48049ce6e18f51574f3ffca5783a0f994e1c8f31
KleinYuan <kaiwen.yuan1992@gmail.com>
2018-05-28 15:17:30 -0700

    Clean up code

--- app/v1.py
+++ app/v1.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import provider
 import importlib
 import numpy as np
 import tensorflow as tf
@@ -7,7 +8,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
-import provider
 
 MODEL_PATH = 'pretrained/log_v1/model.ckpt'
 DATA_PATH = 'kitti/frustum_carpedcyc_val_rgb_detection.pickle'
@@ -19,12 +19,15 @@ NUM_CHANNEL = 4
 fp_nets = importlib.import_module('frustum_pointnets_v1')
 tf.logging.set_verbosity(tf.logging.INFO)
 
+
+
 class FPNetPredictor(object):
 
     graph = tf.Graph()
     sess = None
     saver = None
     ops = None
+
 
     def __init__(self, model_fp):
         tf.logging.info("Initializing FPNetPredictor Instance ...")
@@ -34,6 +37,7 @@ class FPNetPredictor(object):
             self._init_graph()
         tf.logging.info("Initialized FPNetPredictor Instance!")
 
+
     def _init_session(self):
         tf.logging.info("Initializing Session ...")
         with self.graph.as_default():
@@ -41,6 +45,7 @@ class FPNetPredictor(object):
             config.gpu_options.allow_growth = True
             config.allow_soft_placement = True
             self.sess = tf.Session(config=config)
+
 
     def _init_graph(self):
         tf.logging.info("Initializing Graph ...")
@@ -69,6 +74,7 @@ class FPNetPredictor(object):
                    'center': end_points['center'],
                    'end_points': end_points}
 
+
     def predict(self, pc, one_hot_vec):
         tf.logging.info("Predicting with pointcloud and one hot vector ...")
         _ops = self.ops
@@ -85,6 +91,15 @@ class FPNetPredictor(object):
 
         tf.logging.info("Prediction done ! \nResults:\nCenter: {}\nSize Score: {}".format(centers, size_scores))
         return logits, centers, heading_logits, heading_residuals, size_scores, size_residuals
+
+
+def viz(pc):
+    import mayavi.mlab as mlab
+    fig = mlab.figure(figure=None, bgcolor=(0.4,0.4,0.4),
+        fgcolor=None, engine=None, size=(500, 500))
+    mlab.points3d(pc[:,0], pc[:,1], pc[:,2], mode='point',
+        colormap='gnuplot', scale_factor=100, figure=fig)
+    raw_input("Press any key to continue")
 
 
 def test():
@@ -106,12 +121,15 @@ def test():
     prob_list = one_test_data[2]
     one_hot_vec = one_test_data[-1]
     print 'Test data: '
-    print '     Point Cloud Input: ', pc
+    print '     Point Cloud Input Shape: ', pc.shape
     print '     One Hot Vector Input: ', one_hot_vec
     
     print 'Auxiliary data: '
     print '     Rot Angle: ', rot_angle
     print '     Prob List: ', prob_list
+    
+    # Visualization pointcloud
+    viz(pc)
 
     # Data to feed: 1024 points [[x y z int]...[]] and one hot vector [0. 0. 1.] 
     print '     len of point cloud', len(pc)
